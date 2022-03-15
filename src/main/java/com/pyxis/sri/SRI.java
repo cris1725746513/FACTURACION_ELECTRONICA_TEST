a93888f9877717f21551ea15605642ea969081f5
CRISTIAN LEONES <catmanyan@hotmail.com>
2022-03-15 09:16:23 -0500

    main
    inicio

---
+++ src/main/java/com/pyxis/sri/SRI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.pyxis.sri;
+
+/**
+ *
+ * @author Cristian Leones
+ */
+public class SRI {
+
+    public static void main(String[] args) {
+
+    }
+}
