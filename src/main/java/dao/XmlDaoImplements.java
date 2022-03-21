162f8c7b3452abd5cd5134a5f7a3b81acdf49d2f
CRISTIAN LEONES <catmanyan@hotmail.com>
2022-03-21 15:24:15 -0500

    FIRMA

--- src/main/java/dao/XmlDaoImplements.java
+++ src/main/java/dao/XmlDaoImplements.java
@@ -4,6 +4,14 @@
  */
 package dao;
 
+import Utilidad.XAdESBESSignature;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.cert.CertificateException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Cristian Leones
@@ -11,8 +19,18 @@ package dao;
 public class XmlDaoImplements implements XmlInterface{
 
     @Override
-    public void firmar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void firmar(String xmlPath,String pathSignature,String passSignature,String pathOut,String nameFileOut ) {
+        try {
+            XAdESBESSignature.firmar(xmlPath, pathSignature, passSignature, pathOut, nameFileOut);
+            String filePath = pathOut + "\\" + nameFileOut;
+            byte[] bytes = Files.readAllBytes(Paths.get(filePath));
+            System.setProperty("javax.net.ssl.keyStore", "C:\\Program Files (x86)\\Java\\jre1.8.0_261\\lib\\security\\cacerts");
+            System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
+            
+        } catch (CertificateException | IOException ex) {
+            Logger.getLogger(XmlDaoImplements.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
     }
 
     @Override
--- src/main/java/dao/XmlInterface.java
+++ src/main/java/dao/XmlInterface.java
@@ -9,7 +9,7 @@ package dao;
  * @author Cristian Leones
  */
 public interface XmlInterface {
-    void firmar();
+    void firmar(String xmlPath,String pathSignature,String passSignature,String pathOut,String nameFileOut);
     void Autorizar();
     void Guardar();
     
